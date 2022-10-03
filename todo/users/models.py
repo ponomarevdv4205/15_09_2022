7790ffaa2e693221a524b3ab49568f659852feee
user1 <ponomarevdv4205@yandex.ru>
2022-10-03 22:35:51 +0300

    lesson_1

---
+++ todo/users/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+# Create your models here.
+
+class User(models.Model):
+    username = models.CharField(max_length=64)
+    firstname = models.CharField(max_length=64)
+    lastname = models.CharField(max_length=64)
+    # email = models.PositiveIntegerField()
+    email = models.EmailField(('email address'), unique=True,)
