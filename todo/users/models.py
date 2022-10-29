ba8bc87917fde2687a760b00496946f0212e94a5
user1 <ponomarevdv4205@yandex.ru>
2022-10-29 13:50:04 +0300

    lesson_3 29102022_13-49

--- todo/users/models.py
+++ todo/users/models.py
@@ -7,17 +7,25 @@ class User(models.Model):
     username = models.CharField(max_length=128)
     firstname = models.CharField(max_length=128)
     lastname = models.CharField(max_length=128)
-    Email = models.EmailField(('Email address'), unique=True, )
+    email = models.EmailField(('Email address'), unique=True, )
 
     def __str__(self):
         return self.lastname
 
 
-class ToDo(models.Model):
-    text = models.TextField(blank=True, null=True)
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-
-
 class Project(models.Model):
     name = models.CharField(max_length=128)
+    url = models.URLField(default='url')
     users = models.ManyToManyField(User)
+
+    def __str__(self):
+        return self.name
+
+
+class ToDo(models.Model):
+    text = models.TextField(blank=True, null=True)
+    project = models.OneToOneField(Project, on_delete=models.CASCADE, null=True)
+    user = models.OneToOneField(User, on_delete=models.CASCADE, blank=True, null=True)
+    created = models.DateTimeField(auto_now_add=True, null=True)
+    modified = models.DateTimeField(auto_now=True, null=True)
+    is_active = models.BooleanField(default=True, null=True)
