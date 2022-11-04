beaa6dc074e9a5b2fe9c1c04ea9f226b5929f56b
user1 <ponomarevdv4205@yandex.ru>
2022-11-04 17:38:03 +0300

    lesson_6 04.11.2022_17-30

--- todo/todo/urls.py
+++ todo/todo/urls.py
@@ -13,27 +13,64 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+# from django.contrib import admin
+# from django.urls import include, path, re_path
+# from rest_framework.routers import DefaultRouter, SimpleRouter
+# # ,UserModelViewSet, ProjectModelViewSet,
+# from users.views import UserViewSet, ToDoModelViewSet, ProjectDjangoFilterViewSet, \
+#     UserModelViewSet, ProjectModelViewSet, ToDoDjangoFilterViewSet
+# # from rest_framework import permissions
+# # from drf_yasg.views import get_schema_view
+# # from drf_yasg import openapi
+#
+# router = DefaultRouter()
+# # router = SimpleRouter()
+# router.register('users', UserModelViewSet) # Обычное отображение
+# router.register('projects', ProjectModelViewSet) # Обычное отображение
+# router.register('todos', ToDoModelViewSet) # Обычное отображение
+# # router.register('users', UserViewSet) # Настроенное отображение / отображение с фильтрами
+# # router.register('projects', ProjectDjangoFilterViewSet) # Настроенное отображение / отображение с фильтрами
+# # router.register('todos', ToDoDjangoFilterViewSet) # Настроенное отображение / отображение с фильтрами
+#
+# urlpatterns = [
+#     path('admin/', admin.site.urls),
+#     path('api/', include(router.urls)),
+#     path('api-auth/', include('rest_framework.urls')), # Это включает авторизацию
+# ]
+
 from django.contrib import admin
 from django.urls import include, path, re_path
-from rest_framework.routers import DefaultRouter, SimpleRouter
-# ,UserModelViewSet, ProjectModelViewSet,
-from users.views import UserViewSet, ToDoModelViewSet, ProjectDjangoFilterViewSet, \
-    UserModelViewSet, ProjectModelViewSet, ToDoDjangoFilterViewSet
-# from rest_framework import permissions
+from rest_framework.routers import DefaultRouter
+# ,CustomUserModelViewSet, ProjectModelViewSet,
+from users.views import UserViewSet, ToDoModelViewSet, ProjectDjangoFilterViewSet
+from rest_framework import permissions
+from rest_framework.authtoken.views import obtain_auth_token
 # from drf_yasg.views import get_schema_view
 # from drf_yasg import openapi
 
 router = DefaultRouter()
-# router = SimpleRouter()
-router.register('users', UserModelViewSet) # Обычное отображение
-router.register('projects', ProjectModelViewSet) # Обычное отображение
-router.register('todos', ToDoModelViewSet) # Обычное отображение
-# router.register('users', UserViewSet) # Настроенное отображение / отображение с фильтрами
-# router.register('projects', ProjectDjangoFilterViewSet) # Настроенное отображение / отображение с фильтрами
-# router.register('todos', ToDoDjangoFilterViewSet) # Настроенное отображение / отображение с фильтрами
+router.register('users', UserViewSet)
+# router.register('users', CustomUserModelViewSet)
+# router.register('projects', ProjectModelViewSet)
+router.register('projects', ProjectDjangoFilterViewSet)
+router.register('todo', ToDoModelViewSet)
+
+# schema_view = get_schema_view(
+#     openapi.Info(
+#         title="Snippets API",
+#         default_version='v1',
+#         description="Test description",
+#         terms_of_service="https://www.google.com/policies/terms/",
+#         contact=openapi.Contact(email="contact@snippets.local"),
+#         license=openapi.License(name="BSD License"),
+#     ),
+#     public=True,
+#     permission_classes=(permissions.AllowAny,),
+# )
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include(router.urls)),
     path('api-auth/', include('rest_framework.urls')),
+    path('api-token/', obtain_auth_token),
 ]
--- todo/users/views.py
+++ todo/users/views.py
@@ -1,42 +1,75 @@
+from urllib import request
 from .serializers import UserModelSerializer, ProjectModelSerializer, ToDoHyperlinkedModelSerializer
 from rest_framework.viewsets import ModelViewSet, GenericViewSet
-from rest_framework.mixins import ListModelMixin, UpdateModelMixin
+from rest_framework.mixins import ListModelMixin, UpdateModelMixin, RetrieveModelMixin
 from rest_framework.pagination import LimitOffsetPagination
+from rest_framework.permissions import AllowAny, BasePermission
 from .filters import ProjectFilter
 from .models import User, Project, ToDo
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.renderers import JSONRenderer
+from rest_framework.settings import api_settings
 
 
-class UserViewSet(ListModelMixin, UpdateModelMixin, GenericViewSet):
-    # renderer_classes = [JSONRenderer] # Коммент специально, т.к. выводит формат JSON
+class AdminOnly(BasePermission):
+    def has_permission(self, request, view):
+        return request.user.is_superuser
+
+
+class UserViewSet(RetrieveModelMixin,
+                        ListModelMixin,
+                        UpdateModelMixin,
+                        GenericViewSet):
+    # permission_classes = [AllowAny]
     queryset = User.objects.all()
     serializer_class = UserModelSerializer
 
 
-class ToDoDjangoFilterViewSet(ModelViewSet, LimitOffsetPagination):
+# class CustomUserModelViewSet(ModelViewSet):
+#     queryset = CustomUser.objects.all()
+#     serializer_class = CustomUserModelSerializer
+class CustomLimitOffsetPagination(LimitOffsetPagination):
+
+    def __init__(self, default_limit=api_settings.PAGE_SIZE, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.default_limit = default_limit
+
+
+class ViewPaginatorMixin:
+    @property
+    def paginator(self):
+        if not hasattr(self, '_paginator'):
+            if self.pagination_class is None:
+                self._paginator = None
+            else:
+                if getattr(self, 'paginate_limit', None):
+                    self._paginator = self.pagination_class(
+                        default_limit=getattr(self, 'paginate_limit', None))
+                else:
+                    self._paginator = self.pagination_class()
+        return self._paginator
+
+
+class ToDoModelViewSet(ViewPaginatorMixin, ModelViewSet):
     queryset = ToDo.objects.filter(is_active=True)
     serializer_class = ToDoHyperlinkedModelSerializer
     filterset_fields = ['project']
-    limit = LimitOffsetPagination
-    limit.default_limit = 20
-    pagination_class = limit
+    paginate_limit = 2
+    pagination_class = CustomLimitOffsetPagination
 
     def destroy(self, request, *args, **kwargs):
         todo = self.get_object()
         todo.is_active = False
         todo.save()
-        return Response(status=status.HTTP_204_NO_CONTENT)
 
 
-class ProjectDjangoFilterViewSet(ModelViewSet, LimitOffsetPagination):
+class ProjectDjangoFilterViewSet(ViewPaginatorMixin, ModelViewSet):
     queryset = Project.objects.all()
     serializer_class = ProjectModelSerializer
     filterset_class = ProjectFilter
-    limit = LimitOffsetPagination
-    limit.default_limit = 10
-    pagination_class = limit
+    paginate_limit = 2
+    pagination_class = CustomLimitOffsetPagination
 
 
 # -------------------- Обычное отображение -------------------------------------
@@ -51,6 +84,6 @@ class ProjectModelViewSet(ModelViewSet):
     serializer_class = ProjectModelSerializer
 
 
-class ToDoModelViewSet(ModelViewSet):
-    queryset = ToDo.objects.all()
-    serializer_class = ToDoHyperlinkedModelSerializer
+# class ToDoModelViewSet(ModelViewSet):
+#     queryset = ToDo.objects.all()
+#     serializer_class = ToDoHyperlinkedModelSerializer
