a5446b221d2db7f924eb8c8b70cf947317ca7423
Дмитрий Пономарев <91960698+ponomarevdv4205@users.noreply.github.com>
2022-11-04 17:39:34 +0300

    lesson_6 04.11.2022_17-30 (#11)
    ДЗ № 6 по Django REST framework

--- todo/todo/settings.py
+++ todo/todo/settings.py
@@ -44,7 +44,9 @@ INSTALLED_APPS = [
     'rest_framework',
     'django_filters',
     'users',
-    'corsheaders'
+    'corsheaders',
+    # 'drf_yasg',
+    'rest_framework.authtoken',
 ]
 
 MIDDLEWARE = [
@@ -136,5 +138,16 @@ REST_FRAMEWORK = {
     ],
     'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
     # 'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    # 'PAGE_SIZE': 1
+    'PAGE_SIZE': 100,
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated'
+        # 'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+    ],
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        # 'rest_framework.permissions.IsAuthenticated'
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.TokenAuthentication',
+    ]
+
 }
